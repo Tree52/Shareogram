a3e4e901d284ee51066de71801c6270daefca5ab
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-07-26 03:59:16 -0400

    fix tile history behavior

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,11 +21,9 @@
 		if (e.button === 0) isLeftHeld.reset();
 		else if (e.button === 2) isRightHeld.reset();
 		if (clickedTile.value.row !== -1) {
+			if (tilesHistoryIndexer.value !== tilesHistory.value.length - 1)
+				tilesHistory.value.splice(tilesHistoryIndexer.value + 1);
 			tilesHistoryIndexer.value++;
-			if (tilesHistoryIndexer.value !== tilesHistory.value.length - 1) {
-				tilesHistory.value.splice(tilesHistoryIndexer.value);
-				tilesHistoryIndexer.value = tilesHistory.value.length;
-			}
 			tilesHistory.value.push($state.snapshot(tiles.value));
 		}
 		clickedTile.reset();
