07ab7d9211b8ad56e9f96d222ee3abaf2700f1ff
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-09-03 01:17:07 -0400

    chore: deploy command

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+  paths: { base: process.env.NODE_ENV === "production" ? "/Shareogram" : "" },
+  kit: { adapter: adapter() },
+};
 
 export default config;
