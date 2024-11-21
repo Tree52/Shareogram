cf8ccc668ebbf562bd9cd5b68cfb9ee5489eee12
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-11-21 12:36:01 -0500

    refactor: GitHub component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import { tilesSolution, isGame, tiles, win } from "$lib/refs.svelte";
+  import GitHub from "$lib/components/GitHub.svelte";
   import { newEditor } from "$lib/shared.svelte";
 </script>
 
-<header class="items-center flex justify-center">
+<header class="flex items-center justify-center">
+  <GitHub />
+
   {#if isGame.v}
     <span style:padding-top=10px>{win.v ? "You win!" : "Keep trying"}</span>
   {:else}
