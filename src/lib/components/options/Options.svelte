ed0ff23eba950dd5d7b45a32b335ebc1f06263b7
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-11-21 12:21:58 -0500

    fix: thead clipping options

--- src/lib/components/options/Options.svelte
+++ src/lib/components/options/Options.svelte
@@ -18,7 +18,7 @@
 <!-- Note: doing an if and using transition:fly breaks dragscroll for some reason. -->
 <div
   style:right={ sidebarOn.v ? "0" : "-300px" }
-  class="bg-white bottom-0 flex-col fixed flex top-0 w-40 max-w-[75%] rounded-l-3xl overflow-hidden z-10"
+  class="bg-white bottom-0 flex-col fixed flex top-0 w-40 max-w-[75%] rounded-l-3xl overflow-hidden z-50"
 >
   {#if !isGame.v}
     <ImportPNG />
